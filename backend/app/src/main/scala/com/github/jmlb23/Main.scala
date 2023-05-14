9cf4b06725e56f49fc2cbe55a82999dfc2e4ccd2
jmlb23 <rmmenosrf@riseup.net>
2023-05-15 00:02:59 +0200

    use the explicit way

--- backend/app/src/main/scala/com/github/jmlb23/Main.scala
+++ backend/app/src/main/scala/com/github/jmlb23/Main.scala
@@ -2,6 +2,12 @@ package com.github.jmlb23
 
 import cats.effect._
 
-object Main extends IOApp.Simple {
-  val run = IO.println("Hello, World!")
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    args match {
+      case name :: xs =>
+        IO(println(s"Hello, $name.")).as(ExitCode.Success)
+      case Nil =>
+        IO(System.err.println("Usage: MyApp name")).as(ExitCode(2))
+    }
 }
